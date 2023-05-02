6eb57932b1bea336356e9ed4d9f4c6c2114606ce
Mads <mrma19@student.aau.dk>
2023-05-02 15:23:19 +0100

    name update

--- ros2_ws/src/orchestrator/setup.py
+++ ros2_ws/src/orchestrator/setup.py
@@ -3,25 +3,23 @@ import os
 from glob import glob
 
 package_name = 'orchestrator'
-sub_module = 'utils'
+module_utils = 'utils'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name, sub_module],
+    packages=[package_name, module_utils],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name), glob('launch/*.launch.py')),
-        (os.path.join('share', package_name, 'config'), glob('config/*.yaml'))
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    author='Mads',
-    author_email='mrma19@student.aau.dk',
-    maintainer='Anton',
-    maintainer_email='abmo19@student.aau.dk',
+    maintainer='Anton, Mads',
+    maintainer_email='abmo19@student.aau.dk, mrma19@student.aau.dk',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
